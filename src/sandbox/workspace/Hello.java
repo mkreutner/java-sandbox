b2a74b90f087cd1ea81bf7972042358c34b117d1
Michel Kreutner <mkreutner@gmail.com>
2025-08-13 09:14:35 +0200

    chore(init): add sample test code

---
+++ src/sandbox/workspace/Hello.java
@@ -0,0 +1,11 @@
+// Simple Hello World in Java
+// Filename: Hello.java
+// Author: Michel Kreutner
+
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+}

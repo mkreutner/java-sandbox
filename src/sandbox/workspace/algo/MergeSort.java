607b0caa2230ed6817787ead70df8837270573c5
Michel Kreutner <mkreutner@gmail.com>
2025-08-14 11:34:34 +0200

    feat(algo): add merge sort

---
+++ src/sandbox/workspace/algo/MergeSort.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class MergeSort {
+   
+    public static void main(String[] args)
+    {
+        // Retrieve playing data
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter table size:");
+        int n = scanner.nextInt();
+        int[] table = new int[n];
+        System.out.println("Enter table elements:");
+        for (int i = 0; i < n; i++) {
+            table[i] = scanner.nextInt();
+        }
+
+        // Call merge sort methode
+        mergeSort(table, 0, n-1);
+
+        // Display result
+        System.out.println("Sorted table:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(table[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void mergeSort(int[] arr, int left, int right)
+    {
+        if (left < right) {
+            int middle = (left + right) / 2;
+            mergeSort(arr, left, middle);
+            mergeSort(arr, middle + 1, right);
+            merge(arr, left, middle, right);
+        }
+    }
+
+    public static void merge(int[] arr, int left, int middle, int right)
+    {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            leftArray[i] = arr[left + i];
+        }
+        
+        for (int j = 0; j < n2; j++) {
+            rightArray[j] = arr[middle + 1 + j];
+        }
+
+        int i = 0,
+            j = 0,
+            k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+}
---
+++ src/sandbox/workspace/algo/MergeSort.md
@@ -0,0 +1,10 @@
+# Tri fusion (Mergesort) 
+
+Écrivez un programme Java pour trier un tableau d'entiers en utilisant l'algorithme de tri fusion (Mergesort). 
+
+L'algorithme de tri fusion repose sur la méthode de "diviser pour régner" en divisant le tableau en deux moitiés, 
+triant ces moitiés, puis fusionnant les moitiés triées pour obtenir un tableau trié. 
+
+Vous devez coder la logique de tri fusion en Java et vous assurer que votre programme fonctionne correctement. 
+
+Vous pouvez également ajouter des commentaires pour expliquer votre code.

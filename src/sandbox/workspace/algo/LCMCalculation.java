bfed2e49e2d87f3fee33b65a11a00c10760f00b7
Michel Kreutner <mkreutner@gmail.com>
2025-08-15 08:54:54 +0200

    feat(algo): add lcm calculation

---
+++ src/sandbox/workspace/algo/LCMCalculation.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class LCMCalculation {
+
+    private static int firstNumber;
+    private static int secondNumber;
+    private static int gcd;
+    private static int lcm;
+
+    /**
+     * Main fucntion
+     */
+    public static void main(String[] args)
+    {
+        getData();
+        findLCM();
+        displayResult();
+    }
+
+    /**
+     * Display results
+     */
+    public static void displayResult()
+    {
+        System.out.println("The LCM of " + firstNumber + " and " + secondNumber + " is " + lcm);
+        System.out.println();
+    }
+    
+    /**
+     * Get data from user and initialize table
+     */
+    public static void getData()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number (int) : ");
+        firstNumber = scanner.nextInt();
+        System.out.print("Enter second number (int): ");
+        secondNumber = scanner.nextInt();
+        scanner.close();
+    }
+
+    /**
+     * Compute the GCD
+     */
+    public static void findGCD()
+    {
+        int a = firstNumber;
+        int b = secondNumber;
+        while (b != 0 ) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        gcd = a;
+    }
+
+    /**
+     * Compute the LCM
+     */
+    public static void findLCM()
+    {
+        int a = firstNumber;
+        int b = secondNumber;
+
+        findGCD();
+
+        lcm = (a * b) / gcd
+    }
+}
---
+++ src/sandbox/workspace/algo/LCMCalculation.md
@@ -0,0 +1,10 @@
+# Calcul du plus petit commun multiple (PPCM) de deux nombres
+
+Écrivez un programme Java qui calcule le plus petit commun multiple (PPCM) de deux nombres donnés. 
+
+Le PPCM est le plus petit nombre entier positif qui est un multiple commun des deux nombres. 
+
+Vous devez coder la logique de calcul du PPCM en Java et vous assurer que votre programme fonctionne 
+correctement. 
+
+Vous pouvez également ajouter des commentaires pour expliquer votre code.

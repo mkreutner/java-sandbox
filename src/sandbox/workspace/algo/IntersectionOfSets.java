e2038d911c2bae7407f41968730585e0c7942be8
Michel Kreutner <mkreutner@gmail.com>
2025-08-16 21:43:35 +0200

    feat(algo): add intersection of sets

---
+++ src/sandbox/workspace/algo/IntersectionOfSets.java
@@ -0,0 +1,75 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class IntersectionOfSets {
+   
+    private static HashSet<Integer> set1 = new HashSet<Integer>();
+    private static HashSet<Integer> set2 = new HashSet<Integer>();
+    private static HashSet<Integer> intersections = new HashSet<Integer>();
+
+    /**
+     * Main fucntion
+     */
+    public static void main(String[] args)
+    {
+        try {
+            getData();
+            doTheJob();
+            displayResult();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Display results
+     */
+    public static void displayResult()
+    {
+        System.out.println("First set is composed as:");
+        for (Integer elem : set1) {
+            System.out.print(elem.intValue() + " ");
+        }
+        System.out.println();
+        System.out.println("Second set is composed as:");
+        for (Integer elem : set2) {
+            System.out.print(elem.intValue() + " ");
+        }
+        System.out.println();
+        System.out.println("Intersections:");
+        for (Integer elem : intersections) {
+            System.out.print(elem.intValue() + " ");
+        }
+        System.out.println();
+    }
+    
+    /**
+     * Get data from user and initialize table
+     */
+    public static void getData() throws Exception
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter elements of first set (empty to stop): ");
+        String strValue;
+        while (!(strValue = scanner.nextLine()).isEmpty()) {
+            set1.add(Integer.parseInt(strValue));
+        }
+        System.out.print("Enter elements of second set (empty to stop): ");
+        while (!(strValue = scanner.nextLine()).isEmpty()) {
+            set2.add(Integer.parseInt(strValue));
+        }
+        scanner.close();
+    }
+
+    /**
+     * Search intersection
+     */
+    public static void doTheJob()
+    {
+        for (Integer e1 : set1) {
+            if (set2.contains(e1)) {
+                intersections.add(e1);
+            }
+        }
+    }
+}
---
+++ src/sandbox/workspace/algo/IntersectionOfSets.md
@@ -0,0 +1,12 @@
+# Calcul de l'intersection de deux ensembles
+
+Écrivez un programme Java qui calcule l'intersection de deux ensembles d'entiers. 
+
+L'utilisateur doit saisir les éléments des deux ensembles, et le programme doit 
+déterminer et afficher les éléments qui sont présents dans les deux ensembles 
+(c'est-à-dire l'intersection). 
+
+Vous devez coder la logique de calcul de l'intersection des ensembles en Java et 
+vous assurer que votre programme fonctionne correctement. 
+
+Vous pouvez également ajouter des commentaires pour expliquer votre code.

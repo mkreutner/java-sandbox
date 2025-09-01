859f9b50226e5845f2677877f22fcb0be21ae3da
Michel Kreutner <mkreutner@gmail.com>
2025-09-01 15:40:08 +0200

    feat(dyma): chapter 02 - wip

--- src/sandbox/workspace/dyma/Chap02/TypeConvert.java
+++ src/sandbox/workspace/dyma/Chap02/TypeConvert.java
@@ -3,6 +3,7 @@ public class TypeConvert {
     public static void main(String[] args)
     {
         // Implicit Cast
+        System.out.println("");
         System.out.println(">>> Implicit Cast <<<");
 
         byte fromByte = 12;
@@ -18,15 +19,24 @@ public class TypeConvert {
         char fromChar = 'd';
         int toIntFromChar = fromChar;
 
-        System.out.println(String.format("fromChar %s / toIntFromChar = %s", fromChar, toIntFromChar));
+        System.out.println(String.format("fromChar = %s / toIntFromChar = %s", fromChar, toIntFromChar));
 
         // Explicit Cast
-        System.out.println(">>> Implicit Cast <<<");
+        System.out.println("");
+        System.out.println(">>> Explicit Cast <<<");
 
         int fromInt = 102;
         char toCharFromInt = (char) fromInt;
 
-        System.out.println(String.format("fromInt %s / toCharFromInt = %s", fromInt, toCharFromInt));
+        System.out.println(String.format("fromInt = %s / toCharFromInt = %s", fromInt, toCharFromInt));
+
+        System.out.println("");
+        System.out.println(">>> Loose data caused by Cast <<<");
+
+        float fromFloat2 = 12.5f;
+        int toIntFromFloat2 = (int) fromFloat2;
+
+        System.out.println(String.format("fromFloat2 = %s / toIntFromFloat2 = %s", fromFloat2, toIntFromFloat2));
 
     }
 }

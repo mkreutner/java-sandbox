1c156df4bd3b0aafad59bf449bd23524c229ec35
Michel Kreutner <mkreutner@gmail.com>
2025-09-07 09:52:35 +0200

    feat(dyma): chapter 04 - wip

---
+++ src/sandbox/workspace/dyma/Chap04/ParamsAndArgs.java
@@ -0,0 +1,27 @@
+public class ParamsAndArgs {
+
+    public static void main(String[] args) 
+    {
+       System.out.println("Chapter 04 - Introduction to functions."); 
+       System.out.println("23 - Parameters ans arguments.");
+       var result = addition(12, 7);
+       System.out.println(result);
+       System.out.println(concat("Table", "box"));
+       System.out.println(concat("Little", "Thumb", "sows", "white", "pebbles", "to", "be able to", "find", "his", "way"));
+    }
+
+    private static int addition(int a, int b) 
+    {
+        return a + b;
+    }
+
+    private static String concat(String a, String b, String ... extra)
+    {
+        var result = String.format("%s %s", a, b);
+        for (String str : extra) {
+            result = concat(result, str);
+        }
+        return result;
+    }
+
+}

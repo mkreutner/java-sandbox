ee55d98d0d73e7c35a4a8c025dba8e4a9540dc31
Michel Kreutner <mkreutner@gmail.com>
2025-09-11 06:36:59 +0200

    feat(dyma): chapter 06 - wip

---
+++ src/sandbox/workspace/dyma/Chap06/Main.java
@@ -0,0 +1,8 @@
+package sandbox.workspace.dyma.Chap06;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(">>> Chapter 06: Tables and collections");
+    }
+}
